55f562866a60e5d1f111c20798e1ebbb4783a8ec
moa1128 <moa971128@gmail.com>
2023-10-27 16:04:52 +0900

    feat: implement the initial screen

--- ding-todo-ts/pages/index.tsx
+++ ding-todo-ts/pages/index.tsx
@@ -1,12 +1,27 @@
+import * as React from 'react';
+import { useAppSelector, useAppDispatch } from "@/redux/hooks";
+import { add } from "@/redux/store";
+import TodoList from '@/components/todoList';
 import layoutStyles from '@/styles/layout.module.css';
+import TodoTextField from '@/components/todoTextField';
 
 export default function Home() {
+  const dispatch = useAppDispatch();
+  const todoList = useAppSelector(state => state.todo);
+
+  const addTodo = (newTodo: string) => {
+    dispatch(add(newTodo));
+  }
+
   return (
     <div className={layoutStyles.container}>
       <header className={layoutStyles.header}>
         <h2>Ding's TODO List</h2>
       </header>
-      <main></main>
+      <main>
+        <TodoTextField addTodo={addTodo} />
+        <TodoList todoList={todoList} />
+      </main>
     </div>
   )
 }
